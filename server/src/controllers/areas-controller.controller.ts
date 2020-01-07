87f3340d36295f8b3c3b9e0007782754d7d3c66d
Brian gtn <brian.guitteny@epitech.eu>
2020-01-07 18:26:25 +0100

    + Added AreaController + base api routes

---
+++ server/src/controllers/areas-controller.controller.ts
@@ -0,0 +1,65 @@
+import {RestBindings, get, post, patch, del, param, api} from '@loopback/rest';
+
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/context';
+
+@api({basePath: '/areas', paths: {}})
+export class AreasControllerController {
+    constructor() {}
+
+    @get('/')
+    getAreas() {
+
+    }
+
+    @get('/{id}')
+    getArea(
+        @param.path.string('id') id: string
+    ) {
+
+    }
+
+    @post('/')
+    create() {
+
+    }
+
+    @patch('/{id}')
+    update() {
+
+    }
+
+    @del('/{id}')
+    delete() {
+
+    }
+
+    @patch('/enable/{id}')
+    enable(
+        @param.path.string('id') id: string
+    ) {
+    }
+
+    @patch('/disable/{id}')
+    disable(
+        @param.path.string('id') id: string
+    ) {
+    }
+
+    @get('/actions/{area_id}')
+    getActions(
+        @param.path.string('area_id') areaId: string
+    ) {
+
+    }
+
+    @get('/reactions/{area_id}')
+    getReactions(
+        @param.path.string('area_id') areadId: string
+    ) {
+
+    }
+
+}
+
--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './ping.controller';
 export * from './users-controller.controller';
 export * from './services-controller.controller';
+export * from './areas-controller.controller';
