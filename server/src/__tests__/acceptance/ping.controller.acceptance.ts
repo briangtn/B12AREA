68f63d21c8a165f0e60a5c113107506c245ae9d5
briangtn <brian.guitteny@epitech.eu>
2020-01-07 17:57:30 +0100

    Change indent size to 4 & create users api routes

--- server/src/__tests__/acceptance/ping.controller.acceptance.ts
+++ server/src/__tests__/acceptance/ping.controller.acceptance.ts
@@ -3,19 +3,19 @@ import {AreaApplication} from '../..';
 import {setupApplication} from './test-helper';
 
 describe('PingController', () => {
-  let app: AreaApplication;
-  let client: Client;
+    let app: AreaApplication;
+    let client: Client;
 
-  before('setupApplication', async () => {
-    ({app, client} = await setupApplication());
-  });
+    before('setupApplication', async () => {
+        ({app, client} = await setupApplication());
+    });
 
-  after(async () => {
-    await app.stop();
-  });
+    after(async () => {
+        await app.stop();
+    });
 
-  it('invokes GET /ping', async () => {
-    const res = await client.get('/ping?msg=world').expect(200);
-    expect(res.body).to.containEql({greeting: 'Hello from LoopBack'});
-  });
+    it('invokes GET /ping', async () => {
+        const res = await client.get('/ping?msg=world').expect(200);
+        expect(res.body).to.containEql({greeting: 'Hello from LoopBack'});
+    });
 });
