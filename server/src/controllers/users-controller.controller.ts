1934a92b5e0d6b1732c932beb46e0f90a2fe2403
briangtn <brian.guitteny@epitech.eu>
2020-01-07 16:40:08 +0100

    Adding base api server implementation

---
+++ server/src/controllers/users-controller.controller.ts
@@ -0,0 +1,8 @@
+import {RestBindings, get} from '@loopback/rest';
+import {inject} from '@loopback/context';
+// Uncomment these imports to begin using these cool features!
+
+
+export class UsersControllerController {
+  constructor() {}
+}
