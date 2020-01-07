4414bd404268e3b4552a7e20210a5d57bb84f490
Brian gtn <brian.guitteny@epitech.eu>
2020-01-07 18:32:50 +0100

    + Added ReactionsController+ base api routes

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './users-controller.controller';
 export * from './services-controller.controller';
 export * from './areas-controller.controller';
 export * from './actions-controller.controller';
+export * from './reactions-controller.controller';
