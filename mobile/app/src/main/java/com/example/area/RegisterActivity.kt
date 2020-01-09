c740b2a062802d648ce05ae4635e678c3feb4fd2
Jules Bulteau <jules.bulteau@epitech.eu>
2020-01-09 17:13:19 +0100

    add view for registration page

--- mobile/app/src/main/java/com/example/area/RegisterActivity.kt
+++ mobile/app/src/main/java/com/example/area/RegisterActivity.kt
@@ -3,10 +3,10 @@ package com.example.area
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
