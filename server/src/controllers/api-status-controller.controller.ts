e639a60c2dd6c29e1ec0e4b94c5a3508b53eb499
Brian gtn <brian.guitteny@epitech.eu>
2020-01-09 14:00:01 +0100

    + Added /readinessProbe route

---
+++ server/src/controllers/api-status-controller.controller.ts
@@ -0,0 +1,18 @@
+import {RestBindings, Response, get} from '@loopback/rest';
+import {inject} from '@loopback/context';
+
+
+export class ApiStatusControllerController {
+  constructor(@inject(RestBindings.Http.RESPONSE) protected response: Response) {}
+
+  @get('/readinessProbe', {
+    responses: {
+      '200': {
+        description: 'Return a http code 200 if the api is up'
+      }
+    }
+  })
+  isReady(): boolean {
+    return true;
+  }
+}
--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './ping.controller';
 export * from './users-controller.controller';
+export * from './api-status-controller.controller';
