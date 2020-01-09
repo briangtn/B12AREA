c6fafcafa1c1e1e833feb57826ca515b64b2522f
Romain <romain.fouyer@free.fr>
2020-01-09 15:24:50 +0100

    + Added readinessProbe route for continuous deployment

---
+++ web/src/routes/ReadinessProbe.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+interface Props {}
+
+interface State {}
+
+class ReadinessProbe extends Component<Props, State> {
+    render() {
+        return (
+            <div>
+                <img src="img/julian.jpg" alt="Julian best devops" />
+            </div>
+        );
+    }
+}
+
+export default ReadinessProbe;
