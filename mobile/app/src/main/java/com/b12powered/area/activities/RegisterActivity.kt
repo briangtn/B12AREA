d8e6cd4290383e3b09f27b11c7e8060a3230f994
Jules Bulteau <jules.bulteau@epitech.eu>
2020-01-10 17:07:01 +0100

    submit by pressing enter + clear input focus on submit

--- mobile/app/src/main/java/com/b12powered/area/activities/RegisterActivity.kt
+++ mobile/app/src/main/java/com/b12powered/area/activities/RegisterActivity.kt
@@ -9,9 +9,6 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import com.b12powered.area.R
-import kotlinx.android.synthetic.main.activity_register.*
-import org.json.JSONException
-import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -19,36 +16,55 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val etEmail = findViewById<EditText>(R.id.email)
-        val etPassword = findViewById<EditText>(R.id.password)
         val etConfirmPassword = findViewById<EditText>(R.id.confirm_password)
 
         val btnRegister = findViewById<Button>(R.id.register_button)
 
-        btnRegister.setOnClickListener {
-            val email = etEmail.text
-            val password = etPassword.text
-            val confirmPassword = etConfirmPassword.text
+        etConfirmPassword.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
 
-            etEmail.error = null
-            etPassword.error = null
-            etConfirmPassword.error = null
-            if (email.isEmpty()) {
-                etEmail.error = getString(R.string.no_email)
+                register()
             }
-            if (password.isEmpty()) {
-                etPassword.error = getString(R.string.no_password)
-            } else if (confirmPassword.isEmpty()) {
-                etConfirmPassword.error = getString(R.string.no_confirm_password)
-            } else if (password != confirmPassword) {
-                etConfirmPassword.setText("")
-                etConfirmPassword.error = getString(R.string.different_password)
-            }
+            return@OnKeyListener true
+        })
+
+        btnRegister.setOnClickListener {
+            register()
         }
 
     }
 
     private fun register() {
+        val etEmail = findViewById<EditText>(R.id.email)
+        val etPassword = findViewById<EditText>(R.id.password)
+        val etConfirmPassword = findViewById<EditText>(R.id.confirm_password)
+
+        val email = etEmail.text
+        val password = etPassword.text
+        val confirmPassword = etConfirmPassword.text
+
+        etEmail.clearFocus()
+        etPassword.clearFocus()
+        etConfirmPassword.clearFocus()
+
+        etEmail.error = null
+        etPassword.error = null
+        etConfirmPassword.error = null
+
+        if (email.isEmpty()) {
+            etEmail.error = getString(R.string.no_email)
+        }
+        if (password.isEmpty()) {
+            etPassword.error = getString(R.string.no_password)
+        } else if (confirmPassword.isEmpty()) {
+            etConfirmPassword.error = getString(R.string.no_confirm_password)
+        } else if (password != confirmPassword) {
+            etConfirmPassword.setText("")
+            etConfirmPassword.error = getString(R.string.different_password)
+        }
     }
 
 }
