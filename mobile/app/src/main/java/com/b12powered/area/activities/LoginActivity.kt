f13e2122edf14d5af7437e885acfb813920c5c5c
charlie <charlie@localhost.localdomain>
2020-01-13 13:39:25 +0100

    + Add loginActivity mobile page working + Add layout for activity login + Add strings values eng & fr

---
+++ mobile/app/src/main/java/com/b12powered/area/activities/LoginActivity.kt
@@ -0,0 +1,68 @@
+package com.b12powered.area.activities
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import com.b12powered.area.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnRegister = findViewById<Button>(R.id.login_button)
+
+        val etPassword = findViewById<EditText>(R.id.password)
+
+        etPassword.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+
+                submitLogin()
+            }
+            return@OnKeyListener true
+        })
+
+        btnRegister.setOnClickListener {
+            submitLogin()
+        }
+
+    }
+
+    private fun submitLogin() {
+        val etEmail = findViewById<EditText>(R.id.email)
+        val etPassword = findViewById<EditText>(R.id.password)
+
+        val email = etEmail.text
+        val password = etPassword.text
+
+        etEmail.clearFocus()
+        etPassword.clearFocus()
+
+        etEmail.error = null
+        etPassword.error = null
+
+        if (email.isEmpty()) {
+            etEmail.error = getString(R.string.no_email)
+        }
+        if (password.isEmpty()) {
+            etPassword.error = getString(R.string.no_password)
+        }
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            login()
+        }
+    }
+
+    private fun login() {
+
+    }
+
+}
