ce0ca9820b596db8a3036367789a6e4f6a64d251
Julian <julian.frabel@epitech.eu>
2020-01-13 17:43:51 +0100

    fixed linter

--- server/src/services/email.service.config.ts
+++ server/src/services/email.service.config.ts
@@ -3,9 +3,9 @@ import {SmtpOptions} from 'nodemailer-smtp-transport';
 const config: SmtpOptions = {
     "host": process.env.SMTP_HOST ? process.env.SMTP_HOST : "localhost",
     "port": process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 25,
-    "secure": process.env.SMTP_SECURE ? (process.env.SMTP_SECURE == "true") : true,
+    "secure": process.env.SMTP_SECURE ? (process.env.SMTP_SECURE === "true") : true,
     "tls": {
-        "rejectUnauthorized": process.env.SMTP_REJECT_UNAUTHORIZED ? (process.env.SMTP_REJECT_UNAUTHORIZED == "true") : false
+        "rejectUnauthorized": process.env.SMTP_REJECT_UNAUTHORIZED ? (process.env.SMTP_REJECT_UNAUTHORIZED === "true") : false
     },
     "auth": {
         "user": process.env.SMTP_USER,
--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -1,27 +1,27 @@
-import {bind, /* inject, */ BindingScope} from '@loopback/core';
+import {bind} from '@loopback/core';
 import * as nodemailer from 'nodemailer';
 import {config} from "./email.service.config";
 
-export interface IEmail {
-  to: string;
-  from: string;
-  cc?: string;
-  bcc?: string;
-  subject: string;
-  html?: string;
-  text?: string;
+export interface Email {
+    to: string;
+    from: string;
+    cc?: string;
+    bcc?: string;
+    subject: string;
+    html?: string;
+    text?: string;
 }
 
-export interface IEmailManager<T = Object> {
-  sendMail(mailObj: IEmail): Promise<T>;
+export interface EmailManager<T = Object> {
+    sendMail(mailObj: Email): Promise<T>;
 }
 
 @bind({tags: {namespace: "services", name: "email"}})
-export class EmailService implements IEmailManager {
-  constructor() {}
+export class EmailService implements EmailManager {
+    constructor() {}
 
-  async sendMail(mailObj: IEmail): Promise<object> {
-    let transporter = nodemailer.createTransport(config);
-    return await transporter.sendMail(mailObj);
-  }
+    async sendMail(mailObj: Email): Promise<object> {
+        const transporter = nodemailer.createTransport(config);
+        return transporter.sendMail(mailObj);
+    }
 }
