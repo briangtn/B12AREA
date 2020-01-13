7c28359bd1ad3b895549bc4b970824469ff347bc
Brian gtn <brian.guitteny@epitech.eu>
2020-01-13 15:14:16 +0100

    + Adding mongodb datasource

---
+++ server/src/datasources/mongo-db.datasource.config.ts
@@ -0,0 +1,11 @@
+export const config = {
+    "name": "mongo",
+    "connector": "mongodb",
+    "url": "",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "user": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "useNewUrlParser": true
+};
