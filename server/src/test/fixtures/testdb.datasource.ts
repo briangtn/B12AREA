96f352e3d646546abc16b7af8917a91086315810
briangtn <brian.guitteny@epitech.eu>
2020-01-14 18:41:41 +0100

    Test for ci

--- server/src/test/fixtures/testdb.datasource.ts
+++ server/src/test/fixtures/testdb.datasource.ts
@@ -21,6 +21,10 @@ export const testdb: juggler.DataSource = new juggler.DataSource({
 });
 
 before(async function() {
+    if (process.env.CI) {
+        console.log("CI");
+        return;
+    }
     process.env.KUBERNETES_SERVICE_HOST = 'localhost';
     // eslint-disable-next-line no-invalid-this
     this.timeout(30 * 1000);
