f848a9ef6d37c3a847c095813831cad37adc5c09
Julian <julian.frabel@epitech.eu>
2020-01-20 14:42:26 +0100

    hidden twoFactorAuthenticationSecret key

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -2,7 +2,7 @@ import {Entity, model, property} from '@loopback/repository';
 
 @model({
     settings: {
-        hiddenProperties: ['password', 'validationToken', 'resetToken'],
+        hiddenProperties: ['password', 'validationToken', 'resetToken', 'twoFactorAuthenticationSecret'],
         indexes: {
             uniqueEmail: {
                 keys: {
