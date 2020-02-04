c92ca5c534e736422cc7c606d21f615beafe9684
Lilian Arago <lilian.arago@outlook.fr>
2020-02-04 16:21:31 +0100

    Added empty tests

--- server/src/test/api/area/area.controller.test.ts
+++ server/src/test/api/area/area.controller.test.ts
@@ -1,9 +1,9 @@
 import {AreaApplication} from '../../../application';
 import {setupApplication} from '../../acceptance/test-helper';
 import { UserRepository } from '../../../repositories/user.repository';
-import { Client, expect } from '@loopback/testlab';
-import {User} from "../../../models";
+import { Client } from '@loopback/testlab';
 import {AreaRepository} from "../../../repositories";
+import {createUser, getJWT} from "../utils";
 
 describe('/areas', () => {
     let app: AreaApplication;
@@ -47,34 +47,142 @@ describe('/areas', () => {
     });
 
     describe('POST /areas', () => {
+        it('Should create a new area instance', async () => {
 
+        });
+
+        it('Should send a 409 Conflict if an area with the same name exists', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
+
+        it('Should send a 400 Bad Request if there is no name', async () => {
+
+        });
     });
 
     describe('GET /areas', () => {
+        it('Should return an array of areas', async () => {
 
+        });
+
+        it('Should return an empty array if there is none', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 
     describe('GET /areas/count', () => {
+        it('Should return the count of areas', async () => {
+            //TODO Test 0 and X
+        });
 
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 
     describe('GET /areas/{id}', () => {
+        it('Should send the requested area', async () => {
 
+        });
+
+        it('Should send 404 Not Found if the id doesn\'t match', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the area belongs to another user', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 
     describe('DELETE /areas/{id}', () => {
+        it('Should delete the specified area', async () => {
 
+        });
+
+        it('Should send 404 Not Found if the id doesn\'t match', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the area belongs to another user', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 
     describe('PATCH /areas/{id}', () => {
+        it('Should update the specified area', async () => {
 
+        });
+
+        it('Should send 404 Not Found if the id doesn\'t match', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the area belongs to another user', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 
     describe('PATCH /areas/enable/{id}', () => {
+        it('Should enable the specified area', async () => {
 
+        });
+
+        it('Should let enabled the specified area if it was already enabled', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the id doesn\'t match', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the area belongs to another user', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 
     describe('PATCH /areas/disable/{id}', () => {
+        it('Should disable the specified area', async () => {
 
+        });
+
+        it('Should let disabled the specified area if it was already disabled', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the id doesn\'t match', async () => {
+
+        });
+
+        it('Should send 404 Not Found if the area belongs to another user', async () => {
+
+        });
+
+        it('Should send 401 Unauthorized for a request without token', async () => {
+
+        });
     });
 });
