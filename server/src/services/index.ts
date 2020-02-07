67d1bb27ee695afbaf7b749b40e9b6a887272082
Lilian Arago <lilian.arago@outlook.fr>
2020-02-07 14:25:37 +0100

    Reoved area-loder.service

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -4,7 +4,6 @@ export * from './user.service';
 export * from './jwt.service';
 export * from './random-generator.service';
 export * from './two-factor-authentication.service';
-export * from './area-loader.service';
 export * from './exchange-code-generator.service';
 export * from './area-auth-service.service';
 export * from './authorizor';
