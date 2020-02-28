c96f61c49e3236d11378ba5d26d7b3085f388be2
charlie <charlie@localhost.localdomain>
2020-02-28 22:31:03 +0100

    + Add action reaction showing information and deleteReaction

--- mobile/app/src/main/java/com/b12powered/area/activities/ServiceInformationActivity.kt
+++ mobile/app/src/main/java/com/b12powered/area/activities/ServiceInformationActivity.kt
@@ -1,18 +1,22 @@
 package com.b12powered.area.activities
 
 import android.content.Intent
+import android.drm.DrmStore
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.b12powered.area.*
 import com.b12powered.area.api.ApiClient
+import androidx.fragment.app.Fragment
+import com.b12powered.area.fragments.ServiceActionInformationFragment
+import com.b12powered.area.fragments.ServiceReactionInformationFragment
 import com.b12powered.area.fragments.ServiceUserFragment
 
 class ServiceInformationActivity : AppCompatActivity() {
 
     private var _allAreasService : MutableList<Areas>? = mutableListOf()
     private lateinit var listView: ListView
-
+    private var serviceName: String = ""
 
     /**
      * Override method onCreate
@@ -21,72 +25,27 @@ class ServiceInformationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service_information)
 
-        val serviceName = intent.getStringExtra("serviceName")
+        serviceName = intent.getStringExtra("serviceName")
 
         val etServiceName = findViewById<TextView>(R.id.service_name)
 
         etServiceName.hint = serviceName
 
-        getAreasService(serviceName)
-
+        supportFragmentManager.beginTransaction()
+            .add(R.id.create_action_list, ServiceActionInformationFragment.newInstance(serviceName))
+            .commit()
     }
 
-    private fun getAreasService(serviceName: String) {
-        ApiClient(this)
-            .getAreas { areas, message ->
-                if (areas !== null) {
-                    areas.forEach { item ->
-                        if (item.actions != null) {
-                            var actionName = item.actions.serviceAction.substringBefore(".")
-                            if (actionName == serviceName)
-                                    _allAreasService?.add(item)
-                        }
-                    }
-                    printAreas()
-                } else {
-                    Toast.makeText(
-                        this,
-                        message,
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
+    fun changeView(list: Pair<String, ActionDetails>) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.create_action_list, ServiceReactionInformationFragment.newInstance(list))
+            .commit()
     }
 
-    private fun printAreas() {
-        listView = findViewById(R.id.list)
-        var list: ArrayList<String> = ArrayList()
-
-        _allAreasService?.forEach { item ->
-            var serviceAction = item.actions.serviceAction.substringAfterLast(".")
-            var actionOption = ""
-            var reactionOption = ""
-
-            if (item.actions.options != null) {
-                for ((key, value) in item.actions.options)
-                    actionOption = "$actionOption$key : $value\n"
-                list.add(serviceAction.plus(" \n\n") + actionOption)
-            } else {
-                list.add(serviceAction.plus("\n"))
-            }
-
-            if (item.reactions != null) {
-                item.reactions.forEach { second ->
-                    if (second.options != null) {
-                        for ((key, value) in second.options)
-                            reactionOption = "$reactionOption$key : $value\n"
-                        var reactionDetail = second.serviceReaction.substringAfterLast(".")
-                        var reactionName = second.serviceReaction.substringBefore(".")
-                        list.add(reactionName.plus("\t (") + reactionDetail.plus(")\n\n") + reactionOption)
-                    } else {
-                        var reactionDetail = second.serviceReaction.substringAfterLast(".")
-                        var reactionName = second.serviceReaction.substringBefore(".")
-                        list.add(reactionName.plus("\t (") + reactionDetail.plus(")"))
-                    }
-                }
-            }
-        }
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-        listView.adapter = adapter
+    fun refreshView() {
+        intent = Intent(this, ServiceInformationActivity::class.java)
+        intent.putExtra("serviceName", serviceName)
+        finish()
+        startActivity(intent)
     }
 }
---
+++ mobile/app/src/main/java/com/b12powered/area/fragments/ServiceActionInformationFragment.kt
@@ -0,0 +1,213 @@
+package com.b12powered.area.fragments
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.b12powered.area.*
+import com.b12powered.area.activities.ServiceInformationActivity
+import com.b12powered.area.api.ApiClient
+
+/**
+ * The fragment where the user can select and see all actions in specific service type
+ *
+ * This class set a clickable list of actions, depending on service
+ *
+ * @param serviceName The current service clicked
+ */
+class ServiceActionInformationFragment(private val serviceName: String) : Fragment() {
+
+    private var _allAreasService : MutableList<Areas>? = mutableListOf()
+    private lateinit var listView: ListView
+
+    companion object {
+
+        /**
+         * This method return a new instance of [ServiceActionInformationFragment]
+         *
+         * @param serviceName The current service clicked
+         *
+         * @return A new instance of [ServiceActionInformationFragment]
+         */
+        fun newInstance(serviceName: String): ServiceActionInformationFragment {
+            return ServiceActionInformationFragment(serviceName)
+        }
+    }
+
+    /**
+     * Override method onCreateView
+     *
+     * Set the appropriate layout to the current fragment
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_service_action_information, container, false)
+    }
+
+    /**
+     * Override method onViewCreated
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getAreasService(serviceName)
+    }
+
+    /**
+     * Function getAreasService
+     *
+     * Make a getAreas request to the api, using [areas] for get all the action and reaction
+     * @param [serviceName] get all action reaction about a specific service
+     */
+    private fun getAreasService(serviceName: String) {
+        ApiClient(context!!)
+            .getAreas { areas, message ->
+                if (areas !== null) {
+                    areas.forEach { item ->
+                        if (item.actions != null) {
+                            var actionName = item.actions.serviceAction.substringBefore(".")
+                            if (actionName == serviceName)
+                                _allAreasService?.add(item)
+                        }
+                    }
+                    printAreasAction()
+                } else {
+                    Toast.makeText(
+                        context!!,
+                        message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+
+    /**
+     * Function printAreasAction
+     *
+     * Function for print all the current action linked with the service type
+     */
+    private fun printAreasAction() {
+        listView = view!!.findViewById(R.id.list)
+        var list: ArrayList<Pair<String, ActionDetails>> = ArrayList()
+
+        _allAreasService?.forEach { item ->
+            var serviceAction = item.actions.serviceAction.substringAfterLast(".")
+            var actionOption = ""
+
+            if (item.actions.options != null) {
+                for ((key, value) in item.actions.options)
+                    actionOption = "$actionOption$key : $value\n"
+                list.add(Pair(serviceAction.plus("\n\n") + actionOption, item.actions))
+            } else {
+                list.add(Pair(serviceAction.plus("\n"), item.actions))
+            }
+        }
+        val adapter = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, list.map { item -> item.first })
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            showDialog(haveReaction(list[position]), list[position])
+        }
+    }
+
+    /**
+     * Function for switch fragment and show all reactions about an action
+     *
+     * @param list current action selected
+     */
+    private fun showReactionService(list: Pair<String, ActionDetails>) {
+        (activity as ServiceInformationActivity).changeView(list)
+    }
+
+    /**
+     * Function for delete an areas
+     *
+     * @param list current action with areasId to delete
+     */
+    private fun deleteAreas(list : Pair<String, ActionDetails>) {
+        ApiClient(context!!)
+            .deleteArea(list.second.areaId) { success, message ->
+                if (success) {
+                    (activity!! as ServiceInformationActivity).refreshView()
+                } else {
+                    Toast.makeText(
+                        context!!,
+                        message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    /**
+     * Function for showing a dialog when click on specific action
+     *
+     * @param haveReaction True if the action have Reaction false otherwise
+     * @param list The current actionDetails clicked
+     */
+    private fun showDialog(haveReaction: Boolean, list : Pair<String, ActionDetails>) {
+        val builder = AlertDialog.Builder(context)
+        if (haveReaction) {
+            val dialogClickListener = DialogInterface.OnClickListener { dialog, which ->
+                when(which) {
+                    DialogInterface.BUTTON_NEUTRAL -> dialog.dismiss()
+                    DialogInterface.BUTTON_POSITIVE -> showReactionService(list)
+                    DialogInterface.BUTTON_NEGATIVE -> deleteAreas(list)
+                }
+            }
+            builder
+                .setTitle(serviceName)
+                .setMessage(getString(R.string.what_you_want))
+                .setNeutralButton(getString(R.string.cancel), dialogClickListener)
+                .setNegativeButton(getString(R.string.delete_areas), dialogClickListener)
+                .setPositiveButton(getString(R.string.show_reactions), dialogClickListener)
+                .create()
+                .show()
+        } else {
+            val dialogClickListener = DialogInterface.OnClickListener { dialog, which ->
+                when(which) {
+                    DialogInterface.BUTTON_POSITIVE -> deleteAreas(list)
+                    DialogInterface.BUTTON_NEGATIVE -> dialog.dismiss()
+                }
+            }
+            builder
+                .setTitle(serviceName)
+                .setMessage(getString(R.string.what_you_want))
+                .setPositiveButton(getString(R.string.delete_areas), dialogClickListener)
+                .setNegativeButton(getString(R.string.cancel), dialogClickListener)
+                .create()
+                .show()
+
+        }
+    }
+
+    /**
+     * Function for checking if a action have reaction
+     *
+     * @param [list] the current action Clicked
+     * @return true if action contains reaction false otherwise
+     */
+    private fun haveReaction(list : Pair<String, ActionDetails>) : Boolean {
+        _allAreasService?.forEach { item ->
+            if (item.actions.serviceAction == list.second.serviceAction) {
+                if (item.reactions != null) {
+                    item.reactions.forEach { second ->
+                        if (list.second.areaId == second.areaId)
+                            return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+}
